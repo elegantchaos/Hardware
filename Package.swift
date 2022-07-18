b3df0830c2018270c302ca109185be89c6921350
Sam Deane <sam@elegantchaos.com>
2022-07-18 13:39:44 +0100

    Added tool dependencies.

--- Package.swift
+++ Package.swift
@@ -1,22 +1,33 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Hardware",
+    
     products: [
         .library(
             name: "Hardware",
-            targets: ["Hardware"]),
+            targets: ["Hardware"]
+        ),
     ],
+    
     dependencies: [
+        
+        // development tools
+        .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "1.0.0"),
+        .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "1.0.3"),
     ],
+    
     targets: [
         .target(
             name: "Hardware",
-            dependencies: []),
+            dependencies: []
+        ),
+        
         .testTarget(
             name: "HardwareTests",
-            dependencies: ["Hardware"]),
+            dependencies: ["Hardware"]
+        ),
     ]
 )
